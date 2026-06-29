d5bbf7ce7d74cc86bf068194e0d38520730128ea
sannihitha sargu <sargusannihitha@gmail.com.>
2026-06-30 01:17:36 +0530

    connected railway mysql database

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import mysql.connector
 app = Flask(__name__)
 
 db = mysql.connector.connect(
-    host="localhost",
+    host="mysql-4gx.railway.internal",
     user="root",
-    password="@04020808",
-    database="leaveease"
+    password="TREUQuawNtwlzjNPYqxJyGuSedPdEGhK",
+    database="railway",
+    port=3306
 )
 
 cursor = db.cursor()
